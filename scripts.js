529991b89233d81c394e1acc0c0bc845fd2917e9
Nishita1428 <nishita22164063akgec.ac.in>
2023-10-30 00:18:33 +0530

    done

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
    
     function test() {
         var name = document.getElementById("name ").value;
-        var age = document.getElementById("wname ").value;
+        // var age = document.getElementById("wname ").value;
         var email = document.getElementById("ename ").value;
 
         
@@ -10,12 +10,12 @@
         // alert(name+ age + email );
 
     var name = localStorage.setItem("name", name);
-    var wname = localStorage.setItem("age", age);
+    // var wname = localStorage.setItem("age", age);
     var ename = localStorage.setItem("email", email);
 
     var name = localStorage.getItem("name", name);
-    var wname = localStorage.getItem("age", age);
+    // var wname = localStorage.getItem("age", age);
     var ename = localStorage.getItem("email", email);
     
-    window.location.href = "http://127.0.0.1:5501/quiz.html";
+    window.location.href = "quiz.html";
     }
